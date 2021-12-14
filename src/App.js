7cc1357bdba6cd112ff6058fddddb22c84070b19
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-14 11:27:46 +0100

    fetching punks from 3web api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import Header from './components/Header';
 import CollectionCard from './components/CollectionCard';
+import {useState, useEffect} from "react"
+import axios from "axios"
 
 function App() {
+    const [punkListData, setPunkListData] = useState([])
+
+    useEffect(() => {
+      const  getMyNfts = async() => {
+        const openSeaData = await axios.get("https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x489F1f3090Db200864eF266fFbEc7f65538c6f94")
+      console.log(openSeaData.data.assets)
+      setPunkListData(openSeaData.data.assets)
+      }
+      return getMyNfts();
+    }, [])
+  
   return (
     <div className='app' >
       <Header/>
