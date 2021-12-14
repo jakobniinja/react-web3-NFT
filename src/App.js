ded223aa11e4e18733546eb5af0b7adca8e9a203
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-14 10:40:25 +0100

    updating app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import CollectionCard from './components/CollectionCard';
 
 function App() {
   return (
     <div className='app' >
       <Header/>
-    </div>
+     <CollectionCard  id={0} name={"Banda Punk"} traits={[{"value" : 7}]} image="https://ipfs.thirdweb.com/ipfs/bafybeigqkficum3anns36jid3dxvc4yfauyuvqjulbg43n23qxn3ce3tyu" /> 
+      </div>
   );
 }
 
