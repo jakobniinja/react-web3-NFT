150cd4e79355dda59b68429e6c7b42c48b42f0e5
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2021-12-17 14:30:36 +0100

    impl styling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,38 @@ import {useState, useEffect} from "react"
 import axios from "axios"
 import PunkList from './components/PunkList';
 import Main from "./components/Main"
+import PunkList2 from './components/PunkList2';
 
 function App() {
     const [punkListData, setPunkListData] = useState([])
+    const [punkListData2, setPunkListData2] = useState([])
+    const [punkListData3, setPunkListData3] = useState([])
     const [selectedPunk, setselectedPunk] = useState(0)
 
     useEffect(() => {
       const  getMyNfts = async() => {
-        const openSeaData = await axios.get("https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x489F1f3090Db200864eF266fFbEc7f65538c6f94")
-      console.log(openSeaData.data.assets)
+        const openSeaData = await axios.get("https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x4688D81D1Af68AA4fbbFcAc66F97fe25dC86aB9A")
       setPunkListData(openSeaData.data.assets)
       }
       return getMyNfts();
     }, [])
   
+
+    useEffect(() => {
+      const  getMyNfts = async() => {
+        const openSeaData = await axios.get("https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0x8d8b28412DF6a2D13B45B6D0D85D429776792bD7")
+      setPunkListData2(openSeaData.data.assets)
+      }
+      return getMyNfts();
+    }, [])
+    useEffect(() => {
+      const  getMyNfts = async() => {
+        const openSeaData = await axios.get("https://testnets-api.opensea.io/assets?order_direction=asc&asset_contract_address=0xd9b831a4669d87D353966859fc83B777E94Ccb94")
+      console.log(openSeaData.data.assets)
+      setPunkListData3(openSeaData.data.assets)
+      }
+      return getMyNfts();
+    }, [])
   return (
     <div className='app' >
       <Header/>
@@ -29,6 +47,8 @@ function App() {
      <Main selectedPunk={selectedPunk} punkListData={punkListData} />
         
      <PunkList punklistData={punkListData} setselectedPunk={setselectedPunk} />
+          <PunkList2 punklistData={punkListData2}  />
+          <PunkList2 punklistData={punkListData3}  />
           </>
      
      )
